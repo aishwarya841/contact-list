fd2bfac79bfdde2da411858b690768b2987fe22e
Aishwarya Dubey <aishwaryadubey1234@gmail.com>
2021-06-08 22:06:29 +0530

    Added DB

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const port = 8000;
 const path = require('path');
 const fs = require('fs');
 const db = require('./config/mongoose');
+const Contact = require('./model/contact');
 
 const app = express();
-var contactList = [
-    {
-        name : "Aishwarya",
-        number : "227924930"
-    },
-    {
-        name : "Manu",
-        number : "86189406846"
-    }
-];
+// var contactList = [
+//     {
+//         name : "Aishwarya",
+//         number : "227924930"
+//     },
+//     {
+//         name : "Manu",
+//         number : "86189406846"
+//     }
+// ];
 
 
 
@@ -25,28 +26,52 @@ app.use(express.urlencoded());
 app.use(express.static('assets'));
 
 app.get('/',function(req,res){
-    return res.render('home',{
-        title : "Contact List",
-        contact_list : contactList,
-        
-    });
+    Contact.find({},function(err,contact){
+        if(err){
+            console.log("Error in finding the records");
+            return;
+        }
+        return res.render('home',{
+            title : "Contact List",
+            contact_list : contact,
+            
+        });
+
+    })
+    
 
 });
 
 app.post('/create-contact',function(req,res){
-    contactList.push(req.body);
-    return res.redirect('/');
+    Contact.create({
+        name : req.body.name,
+        phone : req.body.phone
+    },function(err, newContact){
+        if(err){
+            console.log("Error while creating the Contact");
+            return;
+        }
+        console.log("***New Contact",newContact);
+        return res.redirect('back');
+    })
 
 });
 
 app.get('/delete-contact',function(req,res){
-    var number = req.query.number;
-    var index = contactList.findIndex(contact => (contact.number == number));
-    if(index != -1){
-        contactList.splice(index,1);
-    }
+    var id = req.query.id;
+    // var index = contactList.findIndex(contact => (contact.number == number));
+    // if(index != -1){
+    //     contactList.splice(index,1);
+    // }
+    Contact.findByIdAndDelete(id,function(err){
+        if(err){
+            console.log("error while deleting the record");
+            return;
+        }
+        return res.redirect('/');
+    });
     
-    return res.redirect('/');
+    
 
 });
 
